e8910bb72281b466f441873c8ad390b1a626d605
Francois Suter <francois@typo3.org>
2016-04-11 17:15:41 +0200

    [TASK] Finalize for publication
    Releases: 2.0
    Resolves: #75521
    Change-Id: I1a3b1740c5a535185b23352e362a7812bcf8b19d
    Reviewed-on: https://review.typo3.org/47571
    Reviewed-by: Francois Suter <francois@typo3.org>
    Tested-by: Francois Suter <francois@typo3.org>

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,46 +1,41 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext: "expressions_sandbox"
-#
-# Auto generated 18-01-2010 13:02
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "expressions_sandbox".
+ *
+ * Auto generated 11-04-2016 17:42
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Testing environment for expressions',
-	'description' => 'Provides a frontend testing area for expressions parsed by the generic expressions parser.',
-	'category' => 'plugin',
-	'author' => 'Francois Suter (Cobweb)',
-	'author_email' => 'typo3@cobweb.ch',
-	'shy' => '',
-	'dependencies' => 'expressions',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'author_company' => '',
-	'version' => '2.0.0-dev',
-	'constraints' => array(
-		'depends' => array(
-			'expressions' => '2.0.0-0.0.0',
-			'typo3' => '6.2.0-7.5.99'
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"c4ef";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"a5fd";s:14:"ext_tables.php";s:4:"697d";s:16:"locallang_db.xml";s:4:"c673";s:19:"doc/wizard_form.dat";s:4:"f547";s:20:"doc/wizard_form.html";s:4:"a924";s:39:"pi1/class.tx_expressionssandbox_pi1.php";s:4:"71c4";s:17:"pi1/locallang.xml";s:4:"bfc4";}',
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Testing environment for expressions',
+  'description' => 'Provides a frontend testing area for expressions parsed by the generic expressions parser.',
+  'category' => 'plugin',
+  'author' => 'Francois Suter (Cobweb)',
+  'author_email' => 'typo3@cobweb.ch',
+  'state' => 'stable',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'author_company' => '',
+  'version' => '2.0.0',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'expressions' => '2.0.0-0.0.0',
+      'typo3' => '7.0.0-7.99.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  '_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"d8cd";s:11:"LICENSE.txt";s:4:"6404";s:10:"README.txt";s:4:"0b61";s:13:"composer.json";s:4:"637b";s:12:"ext_icon.png";s:4:"6a28";s:17:"ext_localconf.php";s:4:"9bcd";s:14:"ext_tables.php";s:4:"3e5a";s:15:"flexform_ds.xml";s:4:"6774";s:16:"locallang_db.xml";s:4:"7c98";s:40:"Classes/Controller/SandboxController.php";s:4:"03f9";s:40:"Resources/Private/Language/locallang.xlf";s:4:"f17b";}',
 );
 
-?>
